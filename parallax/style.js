25422eb2edae59fead01e22449bb5a0bbaa5f622
victorgichohi <vitorgichohi48@gmail.com>
2015-09-16 14:30:10 +0300

    secont parallax folder using jquery only for easy transition

--- parallax/style.js
+++ parallax/style.js
@@ -1,12 +1,12 @@
 (function(){
 
-	var documentEl = $ (document)
-	    paralaxBg = $('div.big-image');
+	var documentEl = $ (document)//new variable
+	    paralaxBg = $('div.big-image,div.big-image2');
 
 	    documentEl.on('scroll',function(){
-	    	var currScrollPos = documentEl.scrollTop();
-	    	parallaxBg.css('background-position','0' + -currScrollPos/4 + 'px');
+	    	var currScrollPos = documentEl.scrollTop();//gets how much scrolled from top in pixels
+	    	parallaxBg.css('background-position','0' + -currScrollPos/4 + 'px');//the minus makes the background move one step up when we scroll.the zero means we dont change the margin.
 	    });
-	    
+
 	   })();
 }
