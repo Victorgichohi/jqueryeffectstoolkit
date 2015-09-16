99a33305fea8dc31075b76c87f3b7cf03c647724
victorgichohi <vitorgichohi48@gmail.com>
2015-09-16 16:31:51 +0300

    final commit,the code is not working that properly,ill revisit it later

--- parallax2/main.js
+++ parallax2/main.js
@@ -1,45 +1,51 @@
 (function($){
-	var $container = $(".paralax");
+	var $container = $(".parallax");
 	 //THIS collect our divs.
-	var $divs = $container.find("div.paralax.background");
+	var $divs = $container.find("div.parallax-background");
    
     /*this checks the height of body
     passes it to thingstobescrolled function*/
-    var thingtobescrolled =document.body;
+    var thingBeingScrolled = document.body;
 
     //get height of each li which we know is 600px
     var liHeight= $divs.eq(0).closest("li").height();
      
-    
-    //defining the various divs well use
-    var i,len,div,li,offset,scroll,top;
-      
-      
+    //getting difference in height
 
-	var render = function(){
+    var diffHeight=$divs.eq(0).height() - liHeight;
+
+         var len=$divs.length;
+    //defining the various divs well use
+    var i,len,div,li,offset,scroll,top,transform;
+      
+       var offsets = $divs.get().map(function(div,d){
+                       return $(div).offset();
+                 });
+
+	var render = function() {
 
 		//what we are scrolling
-       top = thingtobescrolled.scrollTop;
+       top = thingBeingScrolled.scrollTop;
 
            //this function loops through divs
-		for(i=0,len-$divs.length;i<len;i++){
+		for(i=0,len=$divs.length;i<len;i++){
 			//get one div
 			div = $divs[i];
 
 			//get the parent LI
-			li -div.parentNode;
+			li =div.parentNode;
 
 			/*calculate the offsetTop of div
 			which is the position of the top 
 			of the element with reference to the top 
 			of the page*/
-			offset - $(div).offset().top;
+			offset = $(div).offset().top;
 
 			//calculate the amount of scroll
-             scroll - Math.round(((top - offset)/ liHeight) * diffHeight);
+             scroll = Math.round(((top - offset)/ liHeight) * diffHeight);
 
              //apply the scroll amount
-             div.style.webkitTransform - "translate3d(0px, " + scroll +"px,0px)";
+             div.style.webkitTransform ="translate3d(0px, " + scroll +"px,0px)";
 
 
 
@@ -51,6 +57,6 @@
 		requestAnimationFrame(loop);/*this loop allows us to
 		loop through at 100 frameworks per sec*/
 		render();
-	} ();
+	})();
 
-})(jquery();
+})(jquery);
