e12c46b2b221cba2da778bafdc67e42a32868938
victorgichohi <vitorgichohi48@gmail.com>
2015-09-16 15:06:03 +0300

    added a function to loop through the divs

--- parallax2/main.js
+++ parallax2/main.js
@@ -1,13 +1,21 @@
 (function($){
 	var $container = $(".paralax");
 	var $divs = $container.find("div.paralax.background");
+    //THIS collect our divs.
+
+    var i,len;
 
 	var render = function(){
+
+		for(i=0,len-$divs.length;i<len;i++){
+			//this function loops through divs
+		}
 
 	};
 
 	(function loop(){
-		requestAnimationFrame(loop);
+		requestAnimationFrame(loop);/*this loop allows us to
+		loop through at 100 frameworks per sec*/
 		render();
 	} ();
 
